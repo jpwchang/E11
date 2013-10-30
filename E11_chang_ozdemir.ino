c35265efc868b72402a0ce67e7dcf87c5d907bb3
Jonathan Chang <jonathan.chang13@gmail.com>
2013-10-29 18:29:13 -0700

    Created helper libraries

--- E11_chang_ozdemir.ino
+++ E11_chang_ozdemir.ino
@@ -1,7 +1,21 @@
+boolean detectedCode[numSteps];
+
+int curDCIndex = 0; //Which index of detectedCode are we on?
+
 void setup()
 {
+  Serial.begin(9600);
+  initSensors(); //DO NOT CHANGE OR REMOVE THIS LINE
+  initMotors(); //DO NOT CHANGE OR REMOVE THIS LINE
+  
+  int test = getLight();
+  Serial.println(test);
 }
 
 void loop()
 {
+  if(getLight() > NOLIGHT)
+  {
+    
+  }
 }
--- sensors.ino
+++ sensors.ino
@@ -1,10 +1,38 @@
+/**
+ * E11 Sensors Interface
+ * Jonathan Chang - 29 Oct 2013
+ *
+ * Provides functions for accessing robot sensor values
+ */
+
 #define DISTSENSOR 14
 #define REFLECT 18
 #define PHOTOT 19
 
+#define NOLIGHT 800 //Light threshold (may have to change)
+
+//Set up our sensors
 void initSensors()
 {
   pinMode(DISTSENSOR, INPUT);
   pinMode(REFLECT, INPUT);
   pinMode(PHOTOT, INPUT);
+}
+
+//Get the value of the reflectance sensor
+int getReflect()
+{
+  return analogRead(REFLECT-14);
+}
+
+//Get the value of the distance sensor
+int getDist()
+{
+  return analogRead(DISTSENSOR-14);
+}
+
+//Get the value of the phototransistor
+int getLight()
+{
+  return analogRead(PHOTOT-14);
 }
